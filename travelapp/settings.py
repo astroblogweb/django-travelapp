445ba6a1caf26c70162379b4a3967b7e3ee8aafd
astroblogweb <lavanya.vikram.cool@gmail.com>
2017-09-24 16:29:25 +0530

    added welcome

--- travelapp/settings.py
+++ travelapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#    'geoposition',
+    'easy_maps',
+    'placeslist',
 ]
+
+GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyCHYjIn-8XQ_C0X8PfCrgNiRR-BgNBB0Vs'
+EASY_MAPS_GOOGLE_MAPS_API_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ___0123456789'
+EASY_MAPS_CENTER = (-41.3, 32)
+EASY_MAPS_GEOCODE = 'example.custom_geocode'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
