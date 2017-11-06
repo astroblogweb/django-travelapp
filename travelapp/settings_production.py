211b0698781f7c2802ce9d23c19b2e70239ab890
astroblogweb <lavanya.vikram.cool@gmail.com>
2017-11-06 13:01:42 +0530

    prod dev

--- travelapp/settings_production.py
+++ travelapp/settings_production.py
@@ -73,6 +73,6 @@ DATABASES = {
 STATIC_ROOT_SUFFIX="static"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
     #'/var/www/static/',
 ]
