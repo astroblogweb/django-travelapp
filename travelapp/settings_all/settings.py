1c51ee5a772889bd4cee33594360be1d31f4cfa1
astroblogweb <lavanya.vikram.cool@gmail.com>
2017-11-07 18:37:27 +0530

    css for opinions app done

--- travelapp/settings_all/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-if os.environ.get('DJANGO_DEVELOPMENT') is not None:
-    from settings_development import *
-    print("in development mode")
-else:
-    from settings_production import *
-    print("in production mode")
-
-
