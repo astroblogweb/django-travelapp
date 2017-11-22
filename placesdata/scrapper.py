e66cb03b3c234ce752f8a5ec375e60974ae72a56
astroblogweb <lavanya.vikram.cool@gmail.com>
2017-11-22 22:08:15 +0530

    minor changes

--- placesdata/scrapper.py
+++ placesdata/scrapper.py
@@ -27,21 +27,21 @@ def choose_state(site_state,max_listing=100):
 
 def get_state_URL(site_state,max_listing):
     ss=''.join(site_state.split()).lower()
-    print("ss:",ss)
+    # print("ss:",ss)
     query='tripadvisor.in places to visit in'+'_'.join(site_state.split())
     url=[];url_final=""
     for url_gen in search(query, tld='co.in', lang='en',num=20, stop=20,pause=1.0):
         output=re.search(r'^(https://www.tripadvisor.(in|com)/).*(attractions).*(activities).*[^0-9].*(html)',url_gen,re.M|re.I)
-        print(url_gen)
+        # print(url_gen)
         if output:
             url.append(url_gen)
-            print("output exists")
+            # print("output exists")
             final=''.join([x for x in url[-1].split('-')[-1].split('.')[0].split('_')]).lower()
             if final == ss:
-                print("final==ss")
+                # print("final==ss")
                 url_final=url[-1]
                 break
-    print("site_state:",site_state,"\t","final URL:",url_final)
+    # print("site_state:",site_state,"\t","final URL:",url_final)
     return(url_final)
 
 
@@ -51,10 +51,10 @@ def get_state_URL(site_state,max_listing):
 
 def extract_details(site_state,url_final,listing_info,max_listing):
     sites=[]
-    print("l_i:",len(listing_info),max_listing)
+    # print("l_i:",len(listing_info),max_listing)
     for index,listing in enumerate(listing_info):
         if index>max_listing:
-            print("max_listing reached. breaking")
+            # print("max_listing reached. breaking")
             break
         #listing=listing_info[0]
         title=listing.find("div",attrs={"class":"listing_title "})
@@ -64,7 +64,7 @@ def extract_details(site_state,url_final,listing_info,max_listing):
         site_link=listing.find("a")["href"][1:]
         site_link=url_final.split('/')[-2]+'/'+site_link
         site_link='https://'+site_link
-        print("site_link:",site_link)
+        # print("site_link:",site_link)
 #        print(site_link)
         #[site_spend,site_gps]=fetch_hours_GPS(site_link)
         rs_rating=listing.find("div",attrs={"class","rs rating"})
